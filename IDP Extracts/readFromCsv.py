5ea95356dfb6943fd9e47186ec86ef2e7cb09ba2
Saurabh <snawalgaria9@gmail.com>
2016-12-25 17:36:27 +0100

    added VAR readfromCSV

---
+++ IDP Extracts/readFromCsv.py
@@ -0,0 +1,30 @@
+import csv
+import numpy as np
+import pylab as pl
+import scipy.stats as stats
+
+
+def calcVar(filename):
+    daxFile = open(filename)
+    reader = csv.reader(daxFile)
+    data = list(reader)
+    current = []
+    i = 0
+    for d in data:
+        if (i < 2):
+            i = i + 1
+            continue
+        l = d[0].__len__()
+        if (l == 0):
+            continue
+        current.append(float(d[0][0:l - 1]))
+        i = i + 1
+    fit = stats.norm.pdf(current, np.mean(current), np.std(current))
+    pl.plot(current, fit, '-o')
+    pl.hist(current, normed=True)
+    print np.percentile(current, q=5)
+    pl.show()
+
+
+calcVar('wrk1.csv')
+print "hello"
